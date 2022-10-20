8352bbfae9439a3dd4603ad016b467fb4196a3d6
Emma <brantled@mail.uc.edu>
2022-10-20 12:26:13 -0400

    new files

---
+++ game.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    var game = new Phaser.Game();
+    
+}
+var config = {
+    width: 800,
+    height: 600,
+    backgroundColor: 0xff0000,
+}
+var game = new Phaser.Game(config);
