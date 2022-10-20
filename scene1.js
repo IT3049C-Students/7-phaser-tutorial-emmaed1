c1c3852216f26552f502982fcd8753de5e1ade30
Emma <brantled@mail.uc.edu>
2022-10-20 13:27:12 -0400

    finsihed part 5

--- scene1.js
+++ scene1.js
@@ -3,10 +3,27 @@ class Scene1 extends Phaser.Scene{
         super("bootGame");
     }
     preload(){
-        this.load.image("background", "assets/background.png");
-        this.load.image("ship", "assets/ship.png");
-        this.load.image("ship2", "assets/ship2.png");
-        this.load.image("ship3", "assets/ship3.png");
+        this.load.image("background", "assets/images/background.png");
+        this.load.spritesheet("ship", "assests/spritesheets/ship.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship2", "assests/spritesheets/ship2.png", {
+            frameWidth: 32,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship3", "assests/spritesheets/ship3.png", {
+            frameWidth: 32,
+            frameHeight: 32
+        });
+        this.load.spritesheet("explosion", "assests/spritesheets/explosion.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+    
+        this.load.image("ship", "assets/images/ship.png");
+        this.load.image("ship2", "assets/images/ship2.png");
+        this.load.image("ship3", "assets/images/ship3.png");
     }
     create(){
         this.add.text(20, 20, "Loading game...");
--- scene2.js
+++ scene2.js
@@ -10,9 +10,69 @@ class Scene2 extends Phaser.Scene{
         this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
         this.ship2 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
 
+        this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship");
+        this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
+        this.ship2 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
+
+        this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship"),
+            frameRate: 20,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "ship2_anim",
+            frames: this.anims.generateFrameNumbers("ship2"),
+            frameRate: 20,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "ship3_anim",
+            frames: this.anims.generateFrameNumbers("ship3"),
+            frameRate: 20,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hiddenOnComplete: true
+        });
+
+        this.ship1.play("ship1_anim");
+        this.ship2.play("ship2_anim");
+        this.ship3.play("ship3_anim");
+
+        this.ship1.setInteractive();
+        this.ship2.setInteractive();
+        this.ship3.setInteractive();
+
+        this.input.on('gameobjectdown', this.destroyShip, this);
+
         this.add.text(20, 20, "Playing game", {
             font:"25px Arial", 
             fill: "yellow"
         });
+    }
+    update(){
+        this.moveShip(this.ship1, 1);
+        this.moveShip(this.ship2, 2);
+        this.moveShip(this.ship3, 3);
+    }
+    moveShip(ship, speed){
+        ship.y += speed;
+        if(ship.y > config.height){
+            this.resetShipPos(ship);
+        }
+    }
+    resetShipPos(ship){
+        ship.y = 0;
+        var randomX = Pahser.Math.Between(0, config.width);
+        ship.x = randomX
+    }
+    destroyShip(pointer, gameObject){
+        gameObject.setTexture("explosion");
+        gameObject.play("explode");
     }
 }
